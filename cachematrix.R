14d1525cf651899b5cd6b583a567244fc4b37f27
Suresh Rao <sr@sunraa.com>
2015-03-16 21:04:02 -0400

    Added first version of cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+###############################################################################
+## The functions makeCacheMatrix and cacheSolve provide examples of lexical
+## scoping works in R. 
+###############################################################################
 
-## Write a short comment describing this function
+## makeCacheMatrix: This function 'caches' a matrix so that it is available
+##                  for use after it has completed its execution run. 
 
 makeCacheMatrix <- function(x = matrix()) {
+    m <- NULL
 
+    set <- function(y) {
+      x <<- y
+      m <<- NULL
+    }
+    get <- function() x
+    setmatrix <- function(matrix) m <<- matrix
+    getmatrix <- function() m
+    list(set = set, get = get,
+         setmatrix = setmatrix,
+         getmatrix = getmatrix)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve: This function retrieves a matrix from the 'cache' and
+##             calculates the inverse of the matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getmatrix()
+    if(!is.null(m)) {
+      message("getting cached data")
+      return(m)
+    }
+    data <- x$get()
+    m <- solve(data)
+    x$setmatrix(m)
+    m
 }
